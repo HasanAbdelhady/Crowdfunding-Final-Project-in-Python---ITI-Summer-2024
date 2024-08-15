afbbedb7dea97132d19443899f926ad702d8a6ef
Hasan Abdelhady <hasanhany101@gmail.com>
2024-08-15 15:03:31 +0300

    finalized

--- config.py
+++ config.py
@@ -1,56 +1,174 @@
 import json
 from werkzeug.security import check_password_hash
+from login_operations import *
+class User_Operations:
+    def __init__(self, file_path="login.json") -> None:
+        self.file_path = file_path
 
-def save(first_name, last_name, email, password, phone_number):
-    user_data = {}
-    login_credentials = {"email": email, "password": password}
-    user_info = {"first_name": first_name, "last_name": last_name, "email": email, "phone_number": phone_number}
-    id = update_id()
-    user_data[id] = {"login_credentials": login_credentials, "user_info": user_info}
+    def save(self, first_name, last_name, email, password, phone_number):
+        user_data = {}
+        login_credentials = {"email": email, "password": password}
+        user_info = {"first_name": first_name, "last_name": last_name, "email": email, "phone_number": phone_number}
+        id = self.update_id()
+        user_data[id] = {"login_credentials": login_credentials, "user_info": user_info, "user_projects":{}}
 
-    try:
-        with open("login.json", 'r') as file:
-            try:
+        try:
+            with open(self.file_path, 'r') as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = {}
+
+        except FileNotFoundError:
+            data = {}
+            
+        data.update(user_data)
+        with open(self.file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+
+    def update_id(self):
+        try:
+            with open(self.file_path, 'r') as file:
                 data = json.load(file)
-            except json.JSONDecodeError:
-                data = {}
-
-    except FileNotFoundError:
-        data = {}
+                if not data:
+                    return 0
+                return max(int(key) for key in data.keys()) + 1
+        except (FileNotFoundError, json.JSONDecodeError):
+            return 0
         
-    data.update(user_data)
-    with open("login.json", 'w') as file:
-        json.dump(data, file, indent=4)
+    def update_project_id(self,id,inner_key):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = json.load(file)
+                projects = data[id][inner_key]
+                if projects == {}:
+                    return str(0)
+                return max(int(key) for key in data[id][inner_key].keys()) + 1
+        except Exception as e:
+            return e
 
-def update_id():
-    try:
-        with open("login.json", 'r') as file:
-            data = json.load(file)
-            if not data:
-                return 0
-            return max(int(key) for key in data.keys()) + 1
-    except (FileNotFoundError, json.JSONDecodeError):
-        return 0
-
-def verify_user(email):
-    try:
-        with open("login.json", 'r') as login_file:
-            data = json.load(login_file)
+    def verify_user(self, email):
+        try:
+            data = self.read_file()
             for user in data.values():
                 if user["login_credentials"]["email"] == email:
                     return True
-    except FileNotFoundError:
-        return "No user data found"
+        except FileNotFoundError:
+            return "No user data found"
 
-def login(email, password):
-    try:
-        with open("login.json", 'r') as login_file:
-            data = json.load(login_file)
-            for user in data.values():
-                if user["login_credentials"]["email"] == email and check_password_hash(user["login_credentials"]["password"],password):
-                    return "Login successful"
-                else:
-                    continue
-            return "Wrong Credentials"
-    except FileNotFoundError:
-        return "No user data found"
+    def read_file(self):
+        with open(self.file_path, 'r') as file:
+            read_file = json.load(file)
+            return read_file
+        
+    def write_file(self, new_file):
+        with open(self.file_path, 'w') as file:
+            json.dump(new_file, file)
+
+    def login(self, email, password):
+            try:
+                data = self.read_file()
+                user_id = 0
+                    
+                for u_id, user in data.items():
+                    if user["login_credentials"]["email"] == email and check_password_hash(user["login_credentials"]["password"],password):
+                        user_id = u_id
+                        print(self.user_dashboard(user["user_info"]["first_name"], user["user_info"]["last_name"]))
+                        print("""Pick one of the Following Operations:
+                                Create Project => 1
+                                View All Projects => 2
+                                Edit a Project => 3
+                                Delete a Project => 4
+                                Search for a Project using date => 5
+                                """)
+                        break
+                    else:
+                        print("Wrong Credentials")
+                        exit()
+
+                while True:
+                    selection = int(input("Enter your selection: "))
+                    if selection not in range(1,6):
+                        raise ValueError("You can either enter 0 or 1")
+                    
+                    if selection == 1: #Create a new project
+                        project = create_project()
+                        project_id = self.update_project_id(user_id,"user_projects")
+                        data[user_id]["user_projects"][project_id] = project
+
+                        with open(self.file_path, 'w') as login_file:
+                            json.dump(data, login_file)
+
+                        print("Project Created Successfully")
+
+
+                    elif selection == 2: #Show ALl Projcets
+                        projects = data[user_id]["user_projects"]
+                        if projects == {}:
+                            print("You don't have any any projects yet")
+                        else:
+                            data = self.read_file()
+                            projects = data[user_id]["user_projects"]
+                            show_all_projects(projects)
+
+                    elif selection == 3: #Edit Project
+                        if data[user_id]["user_projects"] == {}:
+                            print("You don't have any any projects yet to edit")
+                        else:
+                            show_all_projects(data[user_id]["user_projects"])
+                            project_num = int(input("What project do you want to edit? (Numbers start from 1): ")) - 1
+                            while True:
+                                what_to_edit = int(input(f"""What do you want to edit?
+                                                        1 => Title
+                                                        2 => Details
+                                                        3 => Target
+                                                        4 => Start Date
+                                                        5 => End Date """))
+                                if what_to_edit not in range(6):
+                                    raise ValueError("You can only pick a number from 1 to 5")
+                                else:
+                                    what_to_edit = edit_a_project(what_to_edit)
+                                    break
+                            edits_made = input("Enter your edit: ")
+                            data[user_id]["user_projects"][str(project_num)][what_to_edit] = edits_made
+                            data.update(data)
+                            self.write_file(data)
+                            print("Data Edited Successfully, these are your projects: ")
+                            show_all_projects(data[user_id]["user_projects"])
+
+                    elif selection == 4: #Delete a project
+                        if data[user_id]["user_projects"] == {}:
+                            print("You don't have any any projects yet to Delete")
+                        else:
+                            project_num = int(input("What project do you want to delete? (Numbers start from 1): ")) - 1
+                            data[user_id]["user_projects"].pop(str(project_num))
+                            self.write_file(data)
+                            data = self.read_file()                
+                            print("Project Deleted Successfully, these are your projects: ")
+                            if data[user_id]["user_projects"] != {}:
+                                show_all_projects(data[user_id]["user_projects"])
+                        
+                    elif selection == 5: #Select a Project using its start_date
+                        if data[user_id]["user_projects"] == {}:
+                            print("You don't have any any projects yet to fetch")
+                        else:
+                            date = input("Enter the date in DD/MM/YYYY format: ")
+                            projects = data[user_id]["user_projects"]
+                            for key in projects.keys():
+                                if date == projects[key]["start_date"]:
+                                    print(f"""
+                                            Project Number: {key}
+                                            Project Title: {projects[str(key)]["title"]}
+                                            Project Details: {projects[str(key)]["details"]}
+                                            Project Total Target: {projects[str(key)]["total_target"]}
+                                            Project Start Date: {projects[str(key)]["start_date"]}
+                                            Project Start Date: {projects[str(key)]["end_date"]} """)
+                            
+            except FileNotFoundError:
+                return "No user data found"
+            except ValueError as e:
+                return e
+    
+    def user_dashboard(self,first_name, last_name):
+        return f"Welcome to your Dashboard {first_name} {last_name}"
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import re
 from werkzeug.security import check_password_hash, generate_password_hash
-from config import verify_user, save, login
+from config import User_Operations
 
+#This project might need a Virtual Environment to run properly, if doesn't run normally, this is probably the issue
 while True:
     try:
+        op = User_Operations()
         mode = int(input("Welcome, if you're a new user enter 0, if you already have an account enter 1: "))
         if mode not in range(2):
             raise ValueError("You can either enter 0 or 1")
@@ -12,7 +14,10 @@ while True:
             last_name = input("Enter Your Last Name: ")
             while True:
                 email = input("Enter Your Email: ")
-                if verify_user(email):
+                if re.search(r"@.*\.com$", email) is None:
+                    print("Please enter a valid email")
+                    continue
+                if op.verify_user(email):
                     print("This email is already registered, Please use another email")
                 else:
                     break
@@ -24,12 +29,13 @@ while True:
                 phone_number = input("Enter Your Egyptian Phone Number: ")
                 if re.search("^010|^011|^012|015", phone_number) is not None and len(phone_number) in range(10,12):
                     break
-            save(first_name, last_name, email, password, phone_number)
+            op.save(first_name, last_name, email, password, phone_number)
+            print("User Created Successfully, you can now login")
             break
         elif mode == 1:
             email = input("Enter Your Email: ")
             password = input("Enter Your Password: ")
-            print(login(email, password))
+            op.login(email, password)
             break
 
     except ValueError as v:
